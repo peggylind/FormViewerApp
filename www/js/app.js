8a76edf894997600e897b2d6c7f72eb52a38898f
CarlSteven <carl_steven@live.com>
2015-03-06 14:43:32 -0600

    Fix some small stuff. More mobility.

--- www/js/app.js
+++ www/js/app.js
@@ -53,15 +53,18 @@ databaseModule.config(
                 data : { pageTitle: 'Home' },
                 resolve: {
                     forms: function(formService, $stateParams, $state) {
-                        if(eval($stateParams.rdr))
-                            return formService.getMyForms().then(function(data){
-                                if(data && data.length >0)
-                                    $state.go('form', {id: data[0].id});
-                                else
+                        if (eval($stateParams.rdr)) {
+                            return formService.getMyForms().then(function (data) {
+                                if (!data && !data.length > 0)
                                     return data;
+                                else
+                                    $state.go('form', {id: data[0].id});
                             });
-                        else
-                            return formService.getMyForms();
+                        } else {
+                            return formService.getMyForms().then(function(data){
+                                return data;
+                            });
+                        }
                     }
                 },
                 authenticate: true
@@ -126,6 +129,10 @@ databaseModule.config(
             state('form', {
                 url: "/form/:id",
                 views: {
+                    "menu": {
+                        templateUrl: "partials/menuBar.html",
+                        controller: 'menuCtrl'
+                    },
                     "app": {
                         templateUrl: "partials/form.html",
                         controller: 'formCtrl'
@@ -178,126 +185,126 @@ databaseModule.config(
 
 databaseModule.run(['Restangular', '$rootScope', 'Auth', '$q', '$state', '$builder', 'userService',
     function(Restangular, $rootScope, Auth, $q, $state, $builder, userService) {
-    Restangular.setBaseUrl("https://www.housuggest.org:8443/FormBuilder/");
-    //Restangular.setBaseUrl("http://localhost:8080/RESTFUL-WS/");
+        Restangular.setBaseUrl("https://www.housuggest.org:8443/FormBuilder/");
+        //Restangular.setBaseUrl("http://localhost:8080/RESTFUL-WS/");
 
-    $rootScope.Restangular = function() {
-        return Restangular;
-    };
-    $rootScope.isAuthenticated = function(authenticate) {
-        userService.getMyUser().then(function (result) {
-            $rootScope.uid = result.id.toString();
-            $rootScope.uin = result.username.toString();
-        }, function (error) {
-            if (error.status === 0) { // NO NETWORK CONNECTION OR SERVER DOWN, WE WILL NOT LOG THEM OUT
-                ngNotify.set("Internet or Server Unavailable", {type: "error", sticky: true});
-            } else { //Most Likely a 403 - LOG THEM OUT
-                if (authenticate) {
-                    Auth.clearCredentials();
-                    $state.go("login");
-                    location.reload();
+        $rootScope.Restangular = function() {
+            return Restangular;
+        };
+        $rootScope.isAuthenticated = function(authenticate) {
+            userService.getMyUser().then(function (result) {
+                $rootScope.uid = result.id.toString();
+                $rootScope.uin = result.username.toString();
+            }, function (error) {
+                if (error.status === 0) { // NO NETWORK CONNECTION OR SERVER DOWN, WE WILL NOT LOG THEM OUT
+                    ngNotify.set("Internet or Server Unavailable", {type: "error", sticky: true});
+                } else { //Most Likely a 403 - LOG THEM OUT
+                    if (authenticate) {
+                        Auth.clearCredentials();
+                        $state.go("login");
+                        location.reload();
+                    }
                 }
-            }
-        });
-        return Auth.hasCredentials();
-    };
-    $rootScope.$on("$stateChangeStart", function(event, toState){
-        $('body').removeClass('loaded');
-        // User isn’t authenticated
-        if(toState.name == "form"  && !Auth.hasCredentials()) {
-            Auth.setCredentials("Visitor", "test");
-        } else if (toState.authenticate && !$rootScope.isAuthenticated(toState.authenticate)){
+            });
+            return Auth.hasCredentials();
+        };
+        $rootScope.$on("$stateChangeStart", function(event, toState){
+            $('body').removeClass('loaded');
             // User isn’t authenticated
-            $state.go("login");
-            //Prevents the switching of the state
-            event.preventDefault();
-        }
-        $rootScope.isAuthenticated(false);
-    });
-    $rootScope.$on("$stateChangeSuccess", function(){
-        $('body').addClass('loaded');
-    });
-    $rootScope.$on("$stateChangeError", function(){
-        $('body').addClass('loaded');
-    });
+            if(toState.name == "form"  && !Auth.hasCredentials()) {
+                Auth.setCredentials("Visitor", "test");
+            } else if (toState.authenticate && !$rootScope.isAuthenticated(toState.authenticate)){
+                // User isn’t authenticated
+                $state.go("login");
+                //Prevents the switching of the state
+                event.preventDefault();
+            }
+            $rootScope.isAuthenticated(false);
+        });
+        $rootScope.$on("$stateChangeSuccess", function(){
+            $('body').addClass('loaded');
+        });
+        $rootScope.$on("$stateChangeError", function(){
+            $('body').addClass('loaded');
+        });
 
-    $builder.registerComponent('descriptionHorizontal', {
-        group: 'Other',
-        label: 'TextBlock 1',
-        description: "This is a textblock.",
-        required: false,
-        arrayToText: true,
-        templateUrl: "partials/component/tmplDescriptionHorizontal.html",
-        popoverTemplateUrl: "partials/component/popDescriptionHorizontal.html"
-    });
-    $builder.registerComponent('description', {
-        group: 'Other',
-        label: 'TextBlock 2',
-        description: "This is a textblock.",
-        required: false,
-        arrayToText: true,
-        templateUrl: "partials/component/tmplDescription.html",
-        popoverTemplateUrl: "partials/component/popDescription.html"
-    });
-    $builder.registerComponent('dateTimeInput', {
-        group: 'Other',
-        label: 'Date Time Picker',
-        description: 'Choose a Date and Time',
-        placeholder: '',
-        required: false,
-        templateUrl: 'partials/component/tmplDateTime.html',
-        popoverTemplateUrl: 'partials/component/popDateTime.html'
-    });
-    $builder.registerComponent('dateInput', {
-        group: 'Other',
-        label: 'Date',
-        description: 'Choose a Date',
-        placeholder: '',
-        required: false,
-        templateUrl: 'partials/component/tmplDate.html',
-        popoverTemplateUrl: 'partials/component/popDate.html'
-    });
-    $builder.registerComponent('phoneInput', {
-        group: 'Default',
-        label: 'Phone',
-        description: 'Phone Number',
-        validation: '/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$|^$/',
-        placeholder: '###-###-####',
-        required: false,
-        templateUrl: 'partials/component/tmplPhone.html',
-        popoverTemplateUrl: 'partials/component/popPhone.html'
-    });
-    $builder.registerComponent('name', {
-        group: 'Other',
-        label: 'Name',
-        required: false,
-        arrayToText: true,
-        template: "<div class=\"form-group\">\n    <label for=\"{{formName+index}}\" class=\"col-md-4 control-label\" ng-class=\"{'fb-required':required}\">{{label}}</label>\n    <div class=\"col-md-8\">\n        <input type='hidden' ng-model=\"inputText\" validator-required=\"{{required}}\" validator-group=\"{{formName}}\"/>\n        <div class=\"col-sm-6\" style=\"padding-left: 0;\">\n            <input type=\"text\"\n                ng-model=\"inputArray[0]\"\n                class=\"form-control\" id=\"{{formName+index}}-0\"/>\n            <p class='help-block'>First name</p>\n        </div>\n        <div class=\"col-sm-6\" style=\"padding-left: 0;\">\n            <input type=\"text\"\n                ng-model=\"inputArray[1]\"\n                class=\"form-control\" id=\"{{formName+index}}-1\"/>\n            <p class='help-block'>Last name</p>\n        </div>\n    </div>\n</div>",
-        popoverTemplate: "<form>\n    <div class=\"form-group\">\n        <label class='control-label'>Label</label>\n        <input type='text' ng-model=\"label\" validator=\"[required]\" class='form-control'/>\n    </div>\n    <div class=\"checkbox\">\n        <label>\n            <input type='checkbox' ng-model=\"required\" />\n            Required\n        </label>\n    </div>\n\n    <hr/>\n    <div class='form-group'>\n        <input type='submit' ng-click=\"popover.save($event)\" class='btn btn-primary' value='Close'/>\n        <input type='button' ng-click=\"popover.cancel($event)\" class='btn btn-default' value='Cancel'/>\n        <input type='button' ng-click=\"popover.remove($event)\" class='btn btn-danger' value='Delete'/>\n    </div>\n</form>"
-    });
-    $builder.registerComponent('address', {
-        group: 'Other',
-        label: 'Address',
-        required: false,
-        arrayToText: true,
-        templateUrl: "partials/component/tmplAddress.html",
-        popoverTemplateUrl: "partials/component/popAddress.html"
-    });
-    $builder.registerComponent('section', {
-        group: 'Other',
-        label: 'Section/Page Name',
-        description: 'Conditional Section/Page Description',
-        placeholder: '',
-        required: false,
-        templateUrl: 'partials/component/tmplSection.html',
-        popoverTemplateUrl: 'partials/component/popSection.html'
-    });
-    $builder.registerComponent('fileUpload', {
-        group: 'Other',
-        label: 'Upload a file',
-        description: 'Please Upload a PDF or Word Document File',
-        required: false,
-        templateUrl: 'partials/component/tmplFileUpload.html',
-        popoverTemplateUrl: 'partials/component/popFileUpload.html'
-    });
-}]);
+        $builder.registerComponent('descriptionHorizontal', {
+            group: 'Other',
+            label: 'TextBlock 1',
+            description: "This is a textblock.",
+            required: false,
+            arrayToText: true,
+            templateUrl: "partials/component/tmplDescriptionHorizontal.html",
+            popoverTemplateUrl: "partials/component/popDescriptionHorizontal.html"
+        });
+        $builder.registerComponent('description', {
+            group: 'Other',
+            label: 'TextBlock 2',
+            description: "This is a textblock.",
+            required: false,
+            arrayToText: true,
+            templateUrl: "partials/component/tmplDescription.html",
+            popoverTemplateUrl: "partials/component/popDescription.html"
+        });
+        $builder.registerComponent('dateTimeInput', {
+            group: 'Other',
+            label: 'Date Time Picker',
+            description: 'Choose a Date and Time',
+            placeholder: '',
+            required: false,
+            templateUrl: 'partials/component/tmplDateTime.html',
+            popoverTemplateUrl: 'partials/component/popDateTime.html'
+        });
+        $builder.registerComponent('dateInput', {
+            group: 'Other',
+            label: 'Date',
+            description: 'Choose a Date',
+            placeholder: '',
+            required: false,
+            templateUrl: 'partials/component/tmplDate.html',
+            popoverTemplateUrl: 'partials/component/popDate.html'
+        });
+        $builder.registerComponent('phoneInput', {
+            group: 'Default',
+            label: 'Phone',
+            description: 'Phone Number',
+            validation: '/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$|^$/',
+            placeholder: '###-###-####',
+            required: false,
+            templateUrl: 'partials/component/tmplPhone.html',
+            popoverTemplateUrl: 'partials/component/popPhone.html'
+        });
+        $builder.registerComponent('name', {
+            group: 'Other',
+            label: 'Name',
+            required: false,
+            arrayToText: true,
+            template: "<div class=\"form-group\">\n    <label for=\"{{formName+index}}\" class=\"col-md-4 control-label\" ng-class=\"{'fb-required':required}\">{{label}}</label>\n    <div class=\"col-md-8\">\n        <input type='hidden' ng-model=\"inputText\" validator-required=\"{{required}}\" validator-group=\"{{formName}}\"/>\n        <div class=\"col-sm-6\" style=\"padding-left: 0;\">\n            <input type=\"text\"\n                ng-model=\"inputArray[0]\"\n                class=\"form-control\" id=\"{{formName+index}}-0\"/>\n            <p class='help-block'>First name</p>\n        </div>\n        <div class=\"col-sm-6\" style=\"padding-left: 0;\">\n            <input type=\"text\"\n                ng-model=\"inputArray[1]\"\n                class=\"form-control\" id=\"{{formName+index}}-1\"/>\n            <p class='help-block'>Last name</p>\n        </div>\n    </div>\n</div>",
+            popoverTemplate: "<form>\n    <div class=\"form-group\">\n        <label class='control-label'>Label</label>\n        <input type='text' ng-model=\"label\" validator=\"[required]\" class='form-control'/>\n    </div>\n    <div class=\"checkbox\">\n        <label>\n            <input type='checkbox' ng-model=\"required\" />\n            Required\n        </label>\n    </div>\n\n    <hr/>\n    <div class='form-group'>\n        <input type='submit' ng-click=\"popover.save($event)\" class='btn btn-primary' value='Close'/>\n        <input type='button' ng-click=\"popover.cancel($event)\" class='btn btn-default' value='Cancel'/>\n        <input type='button' ng-click=\"popover.remove($event)\" class='btn btn-danger' value='Delete'/>\n    </div>\n</form>"
+        });
+        $builder.registerComponent('address', {
+            group: 'Other',
+            label: 'Address',
+            required: false,
+            arrayToText: true,
+            templateUrl: "partials/component/tmplAddress.html",
+            popoverTemplateUrl: "partials/component/popAddress.html"
+        });
+        $builder.registerComponent('section', {
+            group: 'Other',
+            label: 'Section/Page Name',
+            description: 'Conditional Section/Page Description',
+            placeholder: '',
+            required: false,
+            templateUrl: 'partials/component/tmplSection.html',
+            popoverTemplateUrl: 'partials/component/popSection.html'
+        });
+        $builder.registerComponent('fileUpload', {
+            group: 'Other',
+            label: 'Upload a file',
+            description: 'Please Upload a PDF or Word Document File',
+            required: false,
+            templateUrl: 'partials/component/tmplFileUpload.html',
+            popoverTemplateUrl: 'partials/component/popFileUpload.html'
+        });
+    }]);
